1e58205d3e96d85ac3d4b086058cfa016601bcf9
sayole <sylee8191@gmail.com>
2023-02-17 02:58:18 +0900

    Initial Commit With Client&Server Config

---
+++ Server/myoverflow/src/main/java/com/preproject/myoverflow/MyoverflowApplication.java
@@ -0,0 +1,13 @@
+package com.preproject.myoverflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyoverflowApplication.class, args);
+	}
+
+}

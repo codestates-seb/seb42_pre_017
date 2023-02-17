5cf6c2cab725106f4b46e5664fc1a7af70768cb4
jellyjw <wldnzzz@naver.com>
2023-02-17 17:43:13 +0900

    feat: 질문 상세페이지 마크업

--- Client/src/components/Post/Buttons.js
+++ Client/src/components/Post/Buttons.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
-export default function Buttons({ text, sx, className, onClick }) {
+export default function Buttons({ text, sx, className, onClick, style }) {
   return (
     <Button sx={sx} variant="outlined" size="medium" className={className} onClick={onClick}>
       {text}
--- Client/src/pages/Post.js
+++ Client/src/pages/Post.js
@@ -3,35 +3,41 @@ import DropDown from "../components/Post/DropDown";
 import Form from "../components/Post/Form";
 import TextEditor from "../components/Post/TextEditor";
 import { GoBook, GoDiffIgnored, GoThreeBars } from "react-icons/go";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Post() {
+  const navigate = useNavigate();
   return (
     <>
       <main className="flex flex-col items-center">
         <div className="mb-[20px]">
           <section className="flex">
-            <GoBook size={24} color="#1B76D2" />
+            <GoBook size={24} />
             <span className="text-[20px] ml-[10px]">제목</span>
           </section>
           <Form sx={{ width: "80vw" }} placeholder="질문 제목을 입력해주세요!" />
         </div>
         <div className="mb-[20px] flex flex-col">
           <section className="flex">
-            <GoThreeBars size={24} color="#1B76D2" />
+            <GoThreeBars size={24} />
             <span className="text-[20px] ml-[10px]">기술 스택</span>
           </section>
           <DropDown sx={{ width: "80vw" }} />
         </div>
         <div className="flex flex-col">
           <section className="flex">
-            <GoDiffIgnored size={24} color="#1B76D2" />
+            <GoDiffIgnored size={24} />
             <span className="text-[20px] ml-[10px]">질문 내용</span>
           </section>
-          <TextEditor className="w-[80vw] mb-[70px]" />
+          <TextEditor className="w-[80vw] mb-[70px]" style={{ height: "50vh" }} />
           <div className="flex justify-end">
-            <Buttons sx={{ width: 140, fontSize: 18, marginRight: "30px" }} text="취소" onClick={<Link to="/" />} />
-            <Buttons sx={{ width: 140, fontSize: 18 }} text="등록" />
+            <Link to="/">
+              <Buttons
+                sx={{ width: 140, fontSize: 18, marginRight: "30px", border: "0.5px solid gray", color: "black" }}
+                text="취소"
+              />
+            </Link>
+            <Buttons sx={{ width: 140, fontSize: 18, border: "0.5px solid gray", color: "black" }} text="등록" />
           </div>
         </div>
       </main>

b220bd13eaee38d1bcbb98486e02bb7f9cb8db9d
sujin <sjij0617@naver.com>
2023-02-27 15:40:09 +0900

    충돌수정2

--- Client/src/components/Home/MyPageDropDown.js
+++ Client/src/components/Home/MyPageDropDown.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from "react";
+import React  from "react";
 import { logout } from '../../util/data';
-
+import { useNavigate } from "react-router-dom";
 export default function MyPageDropDown({onClick,user,onToggle}) {
+  const navigate = useNavigate();
   const handleLoginClick = () =>{
     //실제데이터들어올때 한번더 검증하는 if문 붙일지말지 고민쓰
     onClick()
@@ -14,7 +15,9 @@ export default function MyPageDropDown({onClick,user,onToggle}) {
   return (
     <ul className="absolute top-full right-[-15px] mt-1 z-10 bg-white p-1 text-lg ">
       <li className='cursor-pointer'>내작성글</li>
-      <li className='cursor-pointer'>설정</li>
+      <li onClick={() => navigate("/setting")} className="cursor-pointer">
+        설정
+      </li>
       {user ?
       <li
        onClick={handleLogoutClick}
